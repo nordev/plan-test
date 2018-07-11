f7def0f09aa3e4abd6c7da256011dc7a898cb430
Alvaro Nortes Garcia <anortes@nimgenetics.com>
2018-07-11 18:51:56 +0200

    Added docu and refactor code

--- src/transformer.py
+++ src/transformer.py
@@ -7,7 +7,13 @@ class Transformer:
     def __init__(self):
         pass
 
-    def create_events_info_df(self, src):
+    def create_events_info_df_from_file(self, src):
+        '''
+        Filtra un fichero json con eventos en base a las categorias estipuladas. Se eliminan las lineas duplicadas. Y le
+        otorga una clave unica para su posterior almacenamiento.
+        :param src: Ruta del fichero json de eventos
+        :return: DataFrame con los eventos filtrados
+        '''
         # Set right data type for non string columns
         dtype = {'plan_id': np.int64, 'time': 'datetime64[D]', 'user_id': np.int64}
         df_events = pd.read_json(src, dtype=dtype, encoding='utf-8')
@@ -25,9 +31,14 @@ class Transformer:
             axis=1)
         return df_res
 
-    def create_purchase_detail_df(self, src_json, src_csv):
-        df_plans = pd.read_csv(src_csv, dtype={'id': np.int64}, encoding='utf-8')
-        df_events = self.create_events_info_df(src_json)
+    def create_purchase_detail_df_from_df(self, df_events, df_plans):
+        '''
+        Crea un DataFrame con los detalles de compra. Para ello cruza los DataFrames de eventos y planes,
+         renombra sus columnas y elimina duplicados.
+        :param df_events: DataFrame con eventos
+        :param df_plans: DataFrame con planes
+        :return: Dataframe con detalles de compra
+        '''
 
         # Select only events with plan_id
         df_events_fk = df_events.loc[df_events['plan_id'].notna()].copy()
@@ -44,66 +55,40 @@ class Transformer:
                                                                            "category": "plan_category",
                                                                            "name": "plan_name"})
         df_purchase_detail = df_purchase_detail.drop_duplicates()
-        # df_purchase_detail['user'] = df_purchase_detail['user'].astype(np.int64)
+
         df_purchase_detail['_id'] = df_purchase_detail.apply(
             lambda row: "{}|{}|{}".format(row['user'] if pd.isnull(row['user']) else int(row['user']),
-                                          row['purchase_time'], row['plan_name'].encode('utf-8')),
+                                          row['purchase_time'], row['plan_name']),
             axis=1)
+
+        # Hack for later storing in db. MongoDB doesnot accept pandas type 'NaT'. This value appear when there is an
+        # empty date
+        df_purchase_detail['purchase_time'] = df_purchase_detail.apply(lambda row: "{}".format(
+            str(row['purchase_time']) if row['purchase_time'] == np.datetime64('NaT') else row['purchase_time']),
+                                                                       axis=1)
 
         return df_purchase_detail
 
-    def get_pct_users_search_before_purchase_tasting(self, src_json, src_csv):
-        df_plans = pd.read_csv(src_csv, dtype={'id': np.int64}, encoding='utf-8')
-        df_events = self.create_events_info_df(src_json)
+    def create_purchase_detail_df_from_file(self, src_events, src_plans):
+        '''
+        Idem a la funcion create_purchase_detail_df_from_df pero obtiene como entrada ficheros
+        :param src_events: Ruta al fichero con los eventos
+        :param src_plans: Ruta al ffichero con los planes
+        :return: DataFrame con los detalles de compra
+        '''
+        df_plans = pd.read_csv(src_plans, dtype={'id': np.int64}, encoding='utf-8')
+        return self.create_purchase_detail_df_from_df(src_events, df_plans)
 
-        # Select only events with plan_id
-        df_events_fk = df_events.loc[df_events['plan_id'].notna()].copy()
+    def _write_events_nd_purchase_as_json(self, src_events, src_plans, dst_events, dst_plans):
+        '''
 
-        # Convert again plan_id to int64. Before was an object because there were NaN values
-        df_events_fk['plan_id'] = df_events_fk['plan_id'].astype(np.int64)
+        :param src_events: La ruta origen del fichero de eventos
+        :param src_plans: La ruta origen del fichero de planes
+        :param dst_events: La ruta destino del fichero de planes
+        :param dst_plans: La ruta destino del fichero de planes
+        '''
+        df_events = self.create_events_info_df_from_file(src_events)
+        df_purchase = self.create_purchase_detail_df_from_file(src_events, src_plans)
 
-        # Left join of dfs on primary key plan id.
-        df_merged = df_plans.merge(df_events_fk, left_on='id', right_on='plan_id', how='left')
-        # df_events_fk['plan_id'] = df_events_fk['plan_id'].astype(np.int64)
-        # df_events_fk['user_id'] = df_events_fk['user_id'].astype(np.int64)
-
-        user_purchase = df_merged.loc[
-            (df_merged['category'] == 'tasting') & (df_merged['event'] == 'purchase')].reset_index()
-
-        # Get max date of every purchase by user
-        user_purchase_max = user_purchase[
-            user_purchase.groupby('user_id').time.transform('max') == user_purchase['time']]
-
-        # Which of user_purchase_max had done a search before that date
-        user_search = df_merged.loc[
-            (df_merged['category'] == 'tasting') & (df_merged['event'] == 'plan_view')].reset_index()
-
-        # Get min date of every search by user
-        user_search_min = user_search[user_search.groupby('user_id').time.transform('max') == user_search['time']]
-
-        # Join users that purchase and user that search by user_id and plan id
-        purchase_search_merged = user_purchase_max.merge(user_search_min, left_on=['user_id', 'id'],
-                                                         right_on=['user_id', 'id'], how='inner',
-                                                         suffixes=["_purchase", "_search"])
-
-        # Select user that have searched a plan before purchase it
-        purchase_search_merged['is_lesser'] = purchase_search_merged.apply(
-            lambda row: row['time_search'] < row['time_purchase'], axis=1)
-        purchase_search_merged_ft = purchase_search_merged[["user_id", "id", "time_purchase", "time_search"]].loc[
-            purchase_search_merged['is_lesser']]
-
-        # Number of user that purchased in the category tasting
-        user_purchase_count = user_purchase['user_id'].unique().shape[0]
-
-        # Number of user that had searched before purchase the same plan in the category tasting
-        search_before_purchase_count = purchase_search_merged_ft["user_id"].unique().shape[0]
-
-        # 42.86% of the users have seen the advertiser before make a reservation
-        return 100 * float(search_before_purchase_count) / float(user_purchase_count)
-
-    def _write_events_nd_purchase_as_json(self, src_json, src_csv, dst_json, dst_csv):
-        df_events = self.create_events_info_df(src_json)
-        df_purchase = self.create_purchase_detail_df(src_json, src_csv)
-
-        df_events.to_json(dst_json, orient="records")
-        df_purchase.to_json(dst_csv, orient="records")
+        df_events.to_json(dst_events, orient="records")
+        df_purchase.to_json(dst_plans, orient="records")
--- test/test_transformer.py
+++ test/test_transformer.py
@@ -10,21 +10,18 @@ class TestTransformer(unittest.TestCase):
         self.src_csv = "/opt/repos/plan-test/test/in_files/fever_plans.csv"
 
     def test_create_events_info_df(self):
-        res = self.transformer.create_events_info_df(self.src_json)
-        self.assertEqual(42418,len(res))
+        res = self.transformer.create_events_info_df_from_file(self.src_json)
+        self.assertEqual(42418, len(res))
 
+    # def test_create_purchase_detail_df_from_file(self):
+    #     res = self.transformer.create_purchase_detail_df_from_file(self.src_json, self.src_csv)
+    #     self.assertEqual(13590, len(res))
 
-    def test_create_purchase_detail_df(self):
-        res = self.transformer.create_purchase_detail_df(self.src_json, self.src_csv)
-        self.assertEqual(13590,len(res))
+    # def test__write_events_nd_purchase_as_json(self):
+    #     dst_json = "/opt/repos/plan-test/test/out/events.json"
+    #     dst_csv = "/opt/repos/plan-test/test/out/purchase.json"
+    #     self.transformer._write_events_nd_purchase_as_json(self.src_json, self.src_csv, dst_json, dst_csv)
 
-    # def test_get_pct_users_search_before_purchase_tasting(self):
-    #     print self.transformer.get_pct_users_search_before_purchase_tasting(self.src_json, self.src_csv)
-
-    def test__write_events_nd_purchase_as_json(self):
-        dst_json = "/opt/repos/plan-test/test/out/events.json"
-        dst_csv = "/opt/repos/plan-test/test/out/purchase.json"
-        self.transformer._write_events_nd_purchase_as_json(self.src_json,self.src_csv,dst_json,dst_csv)
 
 if __name__ == '__main__':
     unittest.main()
