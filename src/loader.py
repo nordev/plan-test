dadd8e90a7ab01a245c1bed607056aa7c6df6795
Alvaro Nortes Garcia <anortes@nimgenetics.com>
2018-07-11 18:55:09 +0200

    Upsert one by one function

--- src/loader.py
+++ src/loader.py
@@ -2,27 +2,69 @@ from pymongo import MongoClient
 
 
 class Loader:
+    '''
+    Esta clase se encarga de recibir datos en formato json y subirlos a una base de datos MongoDB.
+    '''
 
     def __init__(self, db_name, db_uri='mongodb://localhost:27017/'):
         self.client = MongoClient(db_uri)
         self.db = self.client[db_name]
 
     def get_collection(self, collection_name):
+        '''
+        Devuelve una coleccion de MongoDB -equivalente a una tabla sql-.
+        :param collection_name:
+        :return: Un cursor a la coleccion
+        '''
         return self.db[collection_name]
 
     def insert_one(self, collection_name, json_elem):
+        '''
+        Inserta un documento -equivalente a una file en sql-.
+        :param collection_name:
+        :param json_elem: Un diccionario o json con los datos del documento.
+        :return:  objeto InsertOneResult permite ver el id del documento insertado.
+        '''
         collection = self.get_collection(collection_name)
         return collection.insert_one(json_elem)
 
     def insert_many(self, collection_name, json_list):
+        '''
+        Permite insertar una lista de diccionarios o jsons en batch.
+        :param collection_name:
+        :param json_list: Lista de diccionarios o jsons.
+        :return: objeto InsertManyResult permite ver los ids del documento insertado.
+        '''
         collection = self.get_collection(collection_name)
         return collection.insert_many(json_list)
 
+    def upsert_many_one_by_one(self, collection_name, json_list):
+        '''
+        Permite insertar una lista de diccionarios o jsons uno por uno. Tiene la particularidad de que actualiza los
+        registros existentes e inserta los que no existen.
+        :param collection_name:
+        :param json_list:
+        '''
+        collection = self.get_collection(collection_name)
+        for document in json_list:
+            id_mongo = document['_id']
+            collection.update_one(filter={'_id': id_mongo}, update={"$set": document}, upsert=True)
 
     def delete_many(self, collection_name, json_query=None):
+        '''
+        Borra docuementos de una coleccion en base a un filtro.
+        :param collection_name:
+        :param json_query: Filtro en forma de quey utilizado
+        :return:
+        '''
         collection = self.get_collection(collection_name)
         return collection.delete_many(json_query)
 
     def count_elems(self, collection_name):
+        '''
+        Cuenta el numero de elementos que hay en una coleccion.
+        :param collection_name:
+        :return: Entero
+        '''
         collection = self.get_collection(collection_name)
         return collection.count()
--- test/test_loader.py
+++ test/test_loader.py
@@ -5,34 +5,13 @@ from src.loader import Loader
 
 class TestLoader(unittest.TestCase):
     def setUp(self):
-        self.loader = Loader(db_uri='mongodb://localhost:27017/', db_name="test_fever")
+        self.loader = Loader(db_uri='mongodb://localhost:27017/', db_name="test_fever_unittest")
         self.coll_purchase = "purchase_detail"
         self.coll_events = "events_info"
 
-    def test_insert_one(self):
-        num_before = self.loader.count_elems(self.coll_events)
-
-        self.loader.insert_one(self.coll_events,
-                               {"city": "MAD", "event": "end_app_session", "os": "ios", "plan_id": "6846",
-                                "time": 1530057600000, "user_id": 611248618,
-                                "_id": "611248618|nan|2018-06-27 00:00:00|end_app_session|ios"})
-
-        num_after = self.loader.count_elems(self.coll_events)
-
-        self.assertEqual(num_after, num_before + 1)
-
-    def test_insert_many_one_by_one(self):
-        num_before = self.loader.count_elems(self.coll_events)
-
-        with open('/opt/repos/plan-test/test/out/events.json') as f:
-            data = json.load(f)
-
-        for json_elem in data:
-            self.loader.insert_one(self.coll_events, json_elem)
-
-        num_after = self.loader.count_elems(self.coll_events)
-
-        self.assertEqual(num_after, num_before + len(data))
+    def tearDown(self):
+        self.loader.delete_many(self.coll_purchase, {})
+        self.loader.delete_many(self.coll_events, {})
 
     def test_insert_many_events(self):
         with open('/opt/repos/plan-test/test/out/events.json') as f:
@@ -46,23 +25,44 @@ class TestLoader(unittest.TestCase):
 
         self.loader.insert_many(self.coll_events, data)
 
-    def test_delete_many_events(self):
+    def test_insert_one(self):
         num_before = self.loader.count_elems(self.coll_events)
-        self.assertTrue(0 != num_before)
 
-        self.loader.delete_many(self.coll_events, {})
+        self.loader.insert_one(self.coll_events,
+                               {"city": "MAD", "event": "end_app_session", "os": "ios", "plan_id": "6846",
+                                "time": 1530057600000, "user_id": 611248618,
+                                "_id": "611248618|nan|2018-06-27 00:00:00|end_app_session|ios"})
 
         num_after = self.loader.count_elems(self.coll_events)
-        self.assertEqual(0, num_after)
 
-    def test_delete_many_purchase(self):
-        num_before = self.loader.count_elems(self.coll_purchase)
-        self.assertTrue(0 != num_before)
+        self.assertEqual(num_after, num_before + 1)
 
-        self.loader.delete_many(self.coll_purchase, {})
+    # def test_insert_many_one_by_one(self):
+    #     num_before = self.loader.count_elems(self.coll_events)
+    #
+    #     with open('/opt/repos/plan-test/test/out/events.json') as f:
+    #         data = json.load(f)
+    #
+    #     for json_elem in data:
+    #         self.loader.insert_one(self.coll_events, json_elem)
+    #
+    #     num_after = self.loader.count_elems(self.coll_events)
+    #
+    #     self.assertEqual(num_after, num_before + len(data))
 
-        num_after = self.loader.count_elems(self.coll_purchase)
-        self.assertEqual(0, num_after)
+    # def test_upsert_many_one_by_one(self):
+    #     jsons = [{"city": "MAD", "event": "end_app_session", "os": "ios", "plan_id": "6846",
+    #      "time": 1530057600000, "user_id": 611248618,
+    #      "_id": "611248618|nan|2018-06-27 00:00:00|end_app_session|ios"},
+    #
+    #     {"city": "MAD", "event": "end_app_session", "os": "ios", "plan_id": "6846",
+    #      "time": 1530057600000, "user_id": 611248618,
+    #      "_id": "ABC|nan|2018-06-27 00:00:00|end_app_session|ios"}]
+    #
+    #     collection = self.loader.get_collection(self.coll_events)
+    #     for document in jsons:
+    #         id_mongo = document['_id']
+    #         collection.update_one(filter={'_id': id_mongo}, update={"$set": document},upsert=True)
 
 
 if __name__ == '__main__':
